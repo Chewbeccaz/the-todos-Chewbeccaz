a0a82f4d0f97d5b3940c15bdea7bdbabc8437c68
Rebecca Hansson <rebeccahansson2016@gmail.com>
2024-01-23 12:47:45 +0100

    added a table-layout.

--- src/components/ShowBooks.tsx
+++ src/components/ShowBooks.tsx
@@ -13,20 +13,53 @@ export const ShowBooks = (props: IShowBooksProps) => {
   return (
     <>
       <h1>TBR-2024</h1>
-      <ul>
+      {/* <ul>
         {props.bookList.map((book) => (
           <li key={book.id}>
             <h3>{book.title}</h3>
             <h4>{book.author}</h4>
             {/* Ska jag skriva funktionerna inuti bookapp...?  */}
-            <input
+      {/* <input
               type="checkbox"
               checked={book.isChecked}
               onChange={() => handleCheckBox(book.id)}
             />
           </li>
         ))}
-      </ul>
+      </ul>  */}
+
+      <table>
+        <thead>
+          <tr>
+            <th>Title:</th>
+            <th>Author:</th>
+            <th>Read:</th>
+            <th>Edit:</th>
+            <th>Delete:</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.bookList.map((book) => (
+            <tr key={book.id}>
+              <td>{book.title}</td>
+              <td>{book.author}</td>
+              <td>
+                <input
+                  type="checkbox"
+                  checked={book.isChecked}
+                  onChange={() => handleCheckBox(book.id)}
+                />
+              </td>
+              <td>
+                <button>ÄndraIkon</button>
+              </td>
+              <td>
+                <button>Sopkorgsikon.</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 };
