0c8e8bf070311b64202f09214d77e7a15d19959e
Rebecca Hansson <rebeccahansson2016@gmail.com>
2024-01-23 15:31:41 +0100

    added a bit of design, icons and bq-img.

--- src/components/ShowBooks.tsx
+++ src/components/ShowBooks.tsx
@@ -2,6 +2,7 @@ import { Book } from "../models/Book";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import "../ShowBooks.css";
 
 interface IShowBooksProps {
   bookList: Book[];
@@ -16,22 +17,7 @@ export const ShowBooks = (props: IShowBooksProps) => {
   return (
     <>
       <h1>TBR-2024</h1>
-      {/* <ul>
-        {props.bookList.map((book) => (
-          <li key={book.id}>
-            <h3>{book.title}</h3>
-            <h4>{book.author}</h4>
-            {/* Ska jag skriva funktionerna inuti bookapp...?  */}
-      {/* <input
-              type="checkbox"
-              checked={book.isChecked}
-              onChange={() => handleCheckBox(book.id)}
-            />
-          </li>
-        ))}
-      </ul>  */}
-
-      <table>
+      <table className="book-table">
         <thead>
           <tr>
             <th>Title:</th>
@@ -42,8 +28,10 @@ export const ShowBooks = (props: IShowBooksProps) => {
           </tr>
         </thead>
         <tbody>
-          {props.bookList.map((book) => (
-            <tr key={book.id}>
+          {props.bookList.map((book, index) => (
+            <tr
+              key={book.id}
+              className={index % 2 === 0 ? "even-row" : "odd-row"}>
               <td>{book.title}</td>
               <td>{book.author}</td>
               <td>
